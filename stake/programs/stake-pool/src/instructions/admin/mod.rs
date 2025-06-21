be804707256207c03ad1cdfe44af11786ad26f8a
DhruvWebDev <artificialintelligencehub35@gmail.com>
2025-06-21 11:37:57 +0000

    Created the file structure for the instructions/admin

---
+++ stake/programs/stake-pool/src/instructions/admin/mod.rs
@@ -0,0 +1,11 @@
+pub mod init_stake_vault;
+pub use init_stake_vault::*;
+
+pub mod collect_fee;
+pub use collect_fee::*;
+
+pub mod init_primary_vault;
+pub use init_primary_vault::*;
+
+pub mod init_fee_account;
+pub use init_fee_account::*;
